117f029d956a1895fead7dab527ef8bbc38b61c3
Daniel Monteagudo <dmonte3210@gmail.com>
2015-11-08 13:54:09 -0500

    Reduced need for admin rights

---
+++ resources/debug.cmd
@@ -0,0 +1,2 @@
+sudo host %1 %2
+pause > NUL
--- resources/host.cmd
+++ resources/host.cmd
@@ -1,11 +1,8 @@
 @ECHO OFF
 SETLOCAL ENABLEEXTENSIONS
 netsh wlan set hostednetwork mode=allow ssid=%1 key=%2
-netsh wlan start hostednetwork 
-echo Hosted Network Started!
 echo SSID: %1
 echo KEY: %2
-echo null > NUL
 IF "%3" NEQ "" (
 	timeout -t %3 /nobreak
 	netsh wlan stop hostednetwork 
